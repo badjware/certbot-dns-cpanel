7e5f018b037e8215a0b23991457a5addbc22d308
Massaki Archambault <badjware@massaki.ca>
2018-04-24 16:13:12 -0400

    credentials file

--- certbot_dns_cpanel/dns_cpanel.py
+++ certbot_dns_cpanel/dns_cpanel.py
@@ -1,30 +1,50 @@
-"""Example Certbot plugins.
-
-For full examples, see `certbot.plugins`.
-
-"""
+"""cPanel dns-01 authenticator plugin"""
 import zope.interface
 
 from certbot import interfaces
 from certbot.plugins import dns_common
 
 
-
 @zope.interface.implementer(interfaces.IAuthenticator)
 @zope.interface.provider(interfaces.IPluginFactory)
 class Authenticator(dns_common.DNSAuthenticator):
-    """Example Authenticator."""
+    """cPanel dns-01 authenticator plugin"""
 
-    description = "Example Authenticator plugin"
+    description = "Obtain a certificate using a DNS TXT record in cPanel"
+    problem = "a"
 
-    # Implement all methods from IAuthenticator, remembering to add
-    # "self" as first argument, e.g. def prepare(self)...
+    def __init__(self, *args, **kwargs):
+        super(Authenticator, self).__init__(*args, **kwargs)
+        self.credentials = None
+
+    @classmethod
+    def add_parser_arguments(cls, add):
+        super(Authenticator, cls).add_parser_arguments(add, default_propagation_seconds=10)
+        add("credentials",
+            type=str,
+            help="The cPanel credentials INI file")
+
+    def more_info(self):
+        return self.description
 
     def _setup_credentials(self):
-        pass
+        self.credentials = self._configure_credentials(
+            'credentials',
+            'The cPanel credentials INI file',
+            {
+                'host': 'cPanel host',
+                'port': 'cPanel port',
+                'accountid': 'cPanel account id',
+                'username': 'cPanel username',
+                'password': 'cPanel password'
+            }
+        )
 
     def _perform(self, domain, validation_domain_name, validation):
         pass
 
     def _cleanup(self, domain, validation_domain_name, validation):
         pass
+
+class _CPanelClient:
+    pass
