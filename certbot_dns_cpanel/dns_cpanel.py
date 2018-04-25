5eb03bc273cc3fda8c6b38f244501bc5e59a0fc7
Massaki Archambault <badjware@massaki.ca>
2018-04-24 21:06:27 -0400

    adding txt record

--- certbot_dns_cpanel/dns_cpanel.py
+++ certbot_dns_cpanel/dns_cpanel.py
@@ -1,9 +1,17 @@
 """cPanel dns-01 authenticator plugin"""
+import logging
+import base64
+import json
+from urllib import request
+from urllib.parse import urlencode
+
 import zope.interface
 
 from certbot import interfaces
 from certbot.plugins import dns_common
 
+
+logger = logging.getLogger(__name__)
 
 @zope.interface.implementer(interfaces.IAuthenticator)
 @zope.interface.provider(interfaces.IPluginFactory)
@@ -41,10 +49,75 @@ class Authenticator(dns_common.DNSAuthenticator):
         )
 
     def _perform(self, domain, validation_domain_name, validation):
-        pass
+        self._get_cpanel_client().add_txt_record(validation_domain_name, validation)
 
     def _cleanup(self, domain, validation_domain_name, validation):
-        pass
+        self._get_cpanel_client().add_txt_record(validation_domain_name, validation)
+
+    def _get_cpanel_client(self):
+        return _CPanelClient(
+            self.credentials.conf('host'),
+            self.credentials.conf('port'),
+            self.credentials.conf('accountid'),
+            self.credentials.conf('username'),
+            self.credentials.conf('password')
+        )
 
 class _CPanelClient:
-    pass
+
+    def __init__(self, host, port, accountid, username, password):
+        self.request_url = "https://%s:%s/json-api/cpanel" % (host, port)
+        self.data = {
+            'cpanel_jsonapi_user': username,
+            'cpanel_jsonapi_apiversion': '2',
+            'cpanel_jsonapi_module': 'ZoneEdit'
+        }
+        self.headers = {
+            'Authorization': 'Basic %s' % str(base64.b64encode(bytes("%s:%s" % (username, password), "utf8")), 'utf8')
+        }
+
+    def add_txt_record(self, record_name, record_content, record_ttl=60):
+        cpanel_domain, cpanel_name = self._get_domain_and_name(record_name)
+
+        data = self.data.copy()
+        data['cpanel_jsonapi_func'] = 'add_zone_record'
+        data['domain'] = cpanel_domain
+        data['name'] = cpanel_name
+        data['type'] = 'TXT'
+        data['txtdata'] = record_content
+        data['ttl'] = record_ttl
+
+        logger.debug(data)
+        with request.urlopen(
+            request.Request(
+                "%s?%s" % (self.request_url, urlencode(data)),
+                headers=self.headers,
+            )
+        ) as response:
+            logger.debug(response.read())
+
+    def del_txt_record(self, record_name, record_content, record_ttl=60):
+        pass
+
+    def _get_domain_and_name(self, record_domain):
+        cpanel_domain = ''
+        cpanel_name = ''
+
+        data = self.data.copy()
+        data['cpanel_jsonapi_func'] = 'fetchzones'
+
+        with request.urlopen(
+            request.Request(
+                "%s?%s" % (self.request_url, urlencode(data)),
+                headers=self.headers
+            )
+        ) as response:
+            for zone in json.load(response)['cpanelresult']['data'][0]['zones']:
+                if record_domain is zone or record_domain.endswith('.' + zone):
+                    cpanel_domain = zone
+                    cpanel_name = record_domain[:-len(zone)-1]
+
+        return (cpanel_domain, cpanel_name)
+
+    def _get_record_number(self, cpanel_domain, cpanel_name):
+        pass
